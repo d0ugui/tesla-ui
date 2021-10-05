7bc6032f6f83d9658b8e3af5cd392d527fab3c9b
Douglas Oliveira <douglaspo_97@outlook.com>
2021-10-04 21:10:09 -0300

    create components and page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Page from './components/Page';
+
 import { GlobalStyls } from './styles/GlobalStyles';
 
 export default function App() {
   return (
     <>
-      <h1>Rocketseat</h1>
+      <Page />
       <GlobalStyls />
     </>
   );
