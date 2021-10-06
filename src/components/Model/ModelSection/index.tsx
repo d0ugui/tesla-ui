3bf65ac155125839359c6cefaff550e5eb7b7985
Douglas Oliveira <douglaspo_97@outlook.com>
2021-10-06 20:09:56 -0300

    create context and useModel hook

--- src/components/Model/ModelSection/index.tsx
+++ src/components/Model/ModelSection/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import useModel from '../useModel';
 import { Container } from './styles';
 
 interface Props extends React.HTMLAttributes<HTMLDivElement> {
@@ -12,7 +13,25 @@ const ModelSection: React.FC<Props> = ({
   children,
   ...props
 }) => {
-  return <Container {...props}>{children}</Container>;
+  const { registerModel } = useModel(modelName);
+
+  const sectionRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (sectionRef.current) {
+      registerModel({
+        modelName,
+        overlayNode,
+        sectionRef
+      });
+    }
+  }, [modelName, overlayNode, registerModel]);
+
+  return (
+    <Container ref={sectionRef} {...props}>
+      {children}
+    </Container>
+  );
 };
 
 export default ModelSection;
