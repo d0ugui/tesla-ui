451bbfad59ad0bb19e734f3b31e45c3fbbd0996a
Douglas Oliveira <douglaspo_97@outlook.com>
2021-10-06 21:13:44 -0300

    create hook useWrapperScroll

--- src/components/Model/ModelOverlay/index.tsx
+++ src/components/Model/ModelOverlay/index.tsx
@@ -1,5 +1,9 @@
+import useWrapperScroll from '../useWrapperScroll';
+
 import { Container } from './styles';
 
 export const ModelOverlay: React.FC = ({ children }) => {
+  const { scrollY } = useWrapperScroll();
+
   return <Container>{children}</Container>;
 };
